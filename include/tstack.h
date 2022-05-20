fda71980247d9659e8029480aa9001f0726eacfc
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-05-20 22:51:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,29 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ public:
+  TStack(): top(-1) {}
+  T get() {
+  return a[top];
+  }
+  bool Empty() {
+  return (top == -1);
+  }
+  bool Full() {
+  return (top == size - 1);
+  }
+  void pop() {
+  if (!Empty())
+  top--;
+  }
+  void push(T x) {
+  if (!Full()) {
+  top++;
+  a[top] = x;
+  }
+  }
+ private:
+  T a[size] = {0};
+  int top = 0;
 };
-
 #endif  // INCLUDE_TSTACK_H_
